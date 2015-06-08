f56555544740a0af04c940553114763aa92678ef
januar_andi <januar@halotec-indonesia.com>
2015-06-08 18:25:59 +0700

    master

---
+++ src/com/aesemailclient/MainActivity.java
@@ -0,0 +1,68 @@
+package com.aesemailclient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+	
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
+	
+	private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    
+    CustomDrawerAdapter adapter;
+    List<DrawerItem> dataList;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		dataList = new ArrayList<DrawerItem>();
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
+                    GravityCompat.START);
+        
+        // Add Drawer Item to dataList
+        dataList.add(new DrawerItem("Message", R.drawable.ic_action_email));
+        dataList.add(new DrawerItem("Likes", R.drawable.ic_action_good));
+        dataList.add(new DrawerItem("Games", R.drawable.ic_action_gamepad));
+        dataList.add(new DrawerItem("Lables", R.drawable.ic_action_labels));
+        dataList.add(new DrawerItem("Search", R.drawable.ic_action_search));
+        dataList.add(new DrawerItem("Cloud", R.drawable.ic_action_cloud));
+        dataList.add(new DrawerItem("Camara", R.drawable.ic_action_camera));
+        dataList.add(new DrawerItem("Video", R.drawable.ic_action_video));
+        dataList.add(new DrawerItem("Groups", R.drawable.ic_action_group));
+        dataList.add(new DrawerItem("Import & Export",
+                    R.drawable.ic_action_import_export));
+        dataList.add(new DrawerItem("About", R.drawable.ic_action_about));
+        dataList.add(new DrawerItem("Settings", R.drawable.ic_action_settings));
+        dataList.add(new DrawerItem("Help", R.drawable.ic_action_help));
+        
+        adapter = new CustomDrawerAdapter(this, R.layout.custom_drawer_item,
+                dataList);
+
+        mDrawerList.setAdapter(adapter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
